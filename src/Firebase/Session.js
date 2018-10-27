8566849fd3105ba90610ac0710fcb62ac8923daa
Ataxia Vision <ataxiavision@gmail.com>
2018-10-27 14:52:02 -0300

    mejora en post fetch

--- src/Firebase/Session.js
+++ src/Firebase/Session.js
@@ -1,5 +1,11 @@
 export function getToken(patientId) {
-    return fetch('https://ataxia-services-project.herokuapp.com/token')
+    let myInitConfiguation = { method: 'POST',
+               body: {
+                patientId : patientId
+               },
+               mode: 'cors',
+               cache: 'default' };
+    return fetch('https://ataxia-services-project.herokuapp.com/token', myInitConfiguation)
     .then(res => res.json(), error => console.error("FALLO AL PEDIR TOKEN"))
     .then(res => {
         return res;
